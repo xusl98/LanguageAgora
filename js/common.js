fcb716525159688aa90790ee010be066fa709bf2
Xusl98 <jesussainz98@gmail.com>
2020-07-24 18:02:40 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,2 +1,2 @@
-// var path = 'http://aglinformatica.es/jsainz/';
-var path = 'http://localhost/LanguageAgora/';
+ var path = 'http://aglinformatica.es/jsainz/';
+//var path = 'http://localhost/LanguageAgora/';
